83d3058c9b1a622c8ba95d535058c1c931f3a82f
Arjan Muka <150122190+armuka13@users.noreply.github.com>
2025-12-04 09:11:17 +0100

    Resovling small issue

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,11 +10,8 @@ class Post extends Model
     
     use HasFactory;
 
-<<<<<<< HEAD
     protected $guarded = []; 
 
-=======
->>>>>>> 3aff404677c0f5c79d07e8827f2a821a3697ef0a
     public function user(){
         return $this->belongsTo(User::class);
     }
